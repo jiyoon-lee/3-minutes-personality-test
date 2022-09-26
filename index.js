a9f0e09cba53c0e02eaf2227c3e59087611ba34b
jiyoon-lee <jiyoon3421@gmail.com>
2022-09-27 00:06:21 +0900

    3분 심리검사 진행중

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-var app = express();
+const app = express();
 const path = require("path");
 
 app.use(express.json());
@@ -14,6 +14,31 @@ app.get("/question", (req, res) => {
   res.sendFile(path.join(__dirname, "src/component/question.html"));
 });
 
+app.get("/result/[1-5]", (req, res) => {
+  res.sendFile(path.join(__dirname, "src/component/result.html"));
+});
+
+app.post("/submit", (req, res) => {
+  const data = req.body;
+  let sumScore = 0;
+  for (let item in data) {
+    sumScore += parseInt(data[item]);
+  }
+  if (sumScore <= 20) {
+    return res.redirect("/result/1");
+  } else if (sumScore <= 30) {
+    return res.redirect("/result/2");
+  } else if (sumScore <= 40) {
+    return res.redirect("/result/3");
+  } else if (sumScore <= 50) {
+    return res.redirect("/result/4");
+  } else if (sumScore <= 60) {
+    return res.redirect("/result/5");
+  } else {
+    return res.redirect("/result/6");
+  }
+});
+
 app.listen(3000, () => {
-  console.log("Server running on 3000!");
+  console.log("Server running on 3000");
 });
--- src/js/question.js
+++ src/js/question.js
@@ -1,4 +1,4 @@
-const main = document.querySelector(".main");
+const questionForm = document.querySelector("#question-form");
 (() => {
   fetch("../data/question.json")
     .then((res) => res.json())
@@ -14,61 +14,144 @@ const main = document.querySelector(".main");
             answerArr.push(answer);
           }
         });
-        main.appendChild(setElement(question, answerArr));
+        questionForm.appendChild(setElement(question, answerArr));
       });
+
+      // 첫번째 문제만 보이도록 한다.
+      const questionItems = document.querySelectorAll(".question-item");
+      const firstQuestionItem = questionItems[0];
+      firstQuestionItem.classList.add("on");
+
+      // 첫번째 문제에서 이전 버튼을 지운다.
+      const prevButtons = document.querySelectorAll(".prev-button");
+
+      prevButtons.forEach((prevButton, index) => {
+        prevButton.addEventListener("click", (e) => {
+          e.preventDefault();
+          const current = questionItems[index];
+          movePrev(current);
+        });
+      });
+      prevButtons[0].style.display = "none";
+
+      const nextButtons = document.querySelectorAll(".next-button");
+      nextButtons.forEach((nextButton, index) => {
+        nextButton.addEventListener("click", (e) => {
+          e.preventDefault();
+          const current = questionItems[index];
+          moveNext(current);
+        });
+      });
+
+      const nextBtns = document.querySelectorAll(".next-btn");
+      nextBtns[nextBtns.length - 1].innerHTML = `
+        <button class="next-button" type="submit">
+          <img src="../images/button-next.png" alt="next-btn">
+        </button>
+      `;
     });
-  const buttonPrev = document.querySelector(".aside-prev");
-  const buttonNext = document.querySelector(".aside-next");
-  buttonNext.addEventListener("click", () => {
-    console.log("asdfasf");
-  });
 })();
 
-const setElement = (question, answerArr) => {
+function setElement(question, answerArr) {
   const questionItem = document.createElement("div");
-  console.log(question, answerArr);
-  // 임시로 템플릿을 저장한다.
+  questionItem.classList.add("question-item");
   const tempContainer = document.createElement("div");
   if (question.pk === 1) {
-    const unorderList = document.createElement("ul");
-    unorderList.classList.add("question-list", "question-list-1");
-    for (const item of answerArr) {
-      unorderList.innerHTML += `
-      <li class="question-item question-item-1 on">
-        <input type="radio" id="answer-1" name="question-1">
-        <label for="answer-1">
-          <figure class="questioin-1-image" style="background-image: url(${item.image});">
-            <img src="${item.image}" alt="question-image">
-          </figure>
-          <div class="image-description">${item.content}</div>
-        </label>
-      </li>
-      `;
-    }
-    tempContainer.innerHTML = unorderList;
-  } else {
-    const unorderList = document.createElement("ul");
-    unorderList.classList.add("question-list");
-    for (const item of answerArr) {
-      unorderList.innerHTML += `
-        <li class="question-item">
-          <input type="radio" name="answer-1" id="answer-2">
-          <label for="answer-2">
-            ${item.content}
+    answerArr.forEach((answer) => {
+      tempContainer.innerHTML += `
+        <li class="answer-item">
+          <input type="radio" id="answer-${answer.pk}" name="question-1" value="${answer.score}">
+          <label for="answer-${answer.pk}">
+            <div class="answer-img" style="background-image: url(${answer.image});"></div>
+            ${answer.content}
+            <span></span>
           </label>
         </li>
       `;
-    }
-    tempContainer.innerHTML = unorderList;
+    });
+  } else if (question.pk === 6) {
+    answerArr.forEach((answer) => {
+      tempContainer.innerHTML += `
+      <li class="answer-item question-6-answer-item">
+        <input type="radio" id="answer-${answer.pk}" name="question-6" value="${answer.score}">
+        <label for="answer-${answer.pk}">
+          <div class="answer-img" style="background-image: url(${answer.image});"></div>
+            ${answer.content}
+          <span></span>
+        </label>
+      </li>
+      `;
+    });
+  } else if (question.pk === 9) {
+    answerArr.forEach((answer) => {
+      tempContainer.innerHTML += `
+      <li class="answer-item">
+        <input type="radio" id="answer-${answer.pk}" name="question-9" value="${answer.score}">
+        <label for="answer-${answer.pk}">
+          <div class="answer-img" style="background-image: url(${answer.image});"></div>
+          <span></span>
+        </label>
+      </li>
+      `;
+    });
+  } else {
+    answerArr.forEach((answer) => {
+      tempContainer.innerHTML += `
+        <li class="answer-item">
+          <input type="radio" id="answer-${answer.pk}" name="question-${question.pk}" value="${answer.score}">
+          <label for="answer-${answer.pk}">
+            <span></span>
+            ${answer.content}
+          </label>
+        </li>
+      `;
+    });
   }
 
   questionItem.innerHTML = `
-    <header class="header"></header>
-    <section class="section"><h2>Q${question.pk}. ${question.content}</h2></section>
-    <article class="article">
-      ${tempContainer}
-    </article>
+    <aside class="prev-btn">
+      <button class="prev-button">
+        <img src="../images/button-prev.png" alt="prev-btn">
+      </button>
+    </aside>
+    <main class="main">
+      <div class="status-bar">
+        <div class="outter-box">
+          <div class="inner-box"></div>
+          <img src="../images/statusbar-icon.png" alt="">
+        </div>
+      </div>
+      <div class="question-box">
+        <h1>Q${question.pk}. ${question.content}</h1>
+        <ul class="answer-list ${
+          [2, 3, 4, 5, 7, 8, 10].includes(question.pk) && "answer-list-string"
+        }">
+          ${tempContainer.innerHTML}
+        </ul>
+      </div>
+    </main>
+    <aside class="next-btn">
+      <button class="next-button">
+        <img src="../images/button-next.png" alt="next-btn">
+      </button>
+    </aside>
   `;
   tempContainer.remove();
   return questionItem;
-};
+}
+
+function movePrev(currentItem) {
+  currentItem.classList.remove("on");
+  let next = currentItem.previousElementSibling;
+  if (next) {
+    next.classList.add("on");
+  }
+}
+
+function moveNext(currentItem) {
+  currentItem.classList.remove("on");
+  let next = currentItem.nextElementSibling;
+  if (next) {
+    next.classList.add("on");
+  }
+}
